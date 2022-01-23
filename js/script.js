db737f64c4e71ab641e57ff8c901c31ad457257d
woong0419 <woong0419@gmail.com>
2022-01-22 20:50:25 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sectionHome = document.querySelector(".home");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHome);
